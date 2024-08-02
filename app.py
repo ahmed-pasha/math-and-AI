d7eef93dcc4346f390f347683e0c4d91b2964097
Ahmed pasha <151767359+ahmed-pasha@users.noreply.github.com>
2024-08-02 09:16:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 
 # Configure Google generative AI
-genai.configure(api_key="AIzaSyB276-hKMAXayPaHj0caoOXApV-1sr503M")  # Replace with your actual API key
+genai.configure(api_key="Your_API_Key")  # Replace with your actual API key
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Initialize the webcam
